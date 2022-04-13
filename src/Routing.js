bd0193966ecbbd617b05b4d7008f37c38b5973d4
Armatova <armatovaaizusha@gmail.com>
2022-04-13 17:36:34 +0600

    footer&orders

--- src/Routing.js
+++ src/Routing.js
@@ -1,12 +1,16 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
+import Orders from "./components/Orders/Orders";
 
 const Routing = () => {
   return (
     <div>
       <BrowserRouter>
         <Header />
+        <Orders />
+        <Footer />
       </BrowserRouter>
     </div>
   );
