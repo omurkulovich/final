585dc686c25dcd10a5cb52f52c7159fde9cb09bc
samarbekova-source <chikasamarbekova@gmail.com>
2022-04-14 12:00:11 +0600

    favorites

--- src/Routing.js
+++ src/Routing.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Cart from "./Cart/Cart";
 import Action from "./components/Action/Action";
 import Add from "./components/Add/Add";
 import AddList from "./components/AddList/AddList";
@@ -29,14 +30,22 @@ const Routing = () => {
     {
       link: "/action",
       element: <Action />,
+      id: 4,
     },
     {
       link: "/add",
       element: <Add />,
+      id: 5,
     },
     {
       link: "/addcard",
       element: <AddList />,
+      id: 6,
+    },
+    {
+      link: "/cart",
+      element: <Cart />,
+      id: 7,
     },
   ];
 
